49425caf3f7f42b7a29c8f2d639e305779d09a65
Sumeet Suryawanshi <s.l.suryawanshi4@gmail.com>
2024-11-11 19:01:13 -0700

    Added Loading Screen and scene transition logic

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,16 +2,30 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform playerTransform;
-    public Vector3 offset = new Vector3(0, 10, -10); // Default offset from the player
-    public Vector3 cameraRotation = new Vector3(37.5f, 0, 0); // Default rotation for the camera
+    public static CameraFollow Instance;
+
+    public Vector3 offset = new Vector3(0, 10, -10);
+    public Vector3 cameraRotation = new Vector3(37.5f, 0, 0);
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);  // Persist camera across scenes
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void LateUpdate()
     {
-        // Set the camera position to follow the player
-        transform.position = playerTransform.position + offset;
-
-        // Apply custom rotation to the camera
-        transform.rotation = Quaternion.Euler(cameraRotation);
+        if (PlayerMovement.Instance != null)
+        {
+            transform.position = PlayerMovement.Instance.transform.position + offset;
+            transform.rotation = Quaternion.Euler(cameraRotation);
+        }
     }
 }
