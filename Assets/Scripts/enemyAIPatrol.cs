415a9b3f65dee0d4f3a52095de3df1642a41a47d
Sumeet Suryawanshi <s.l.suryawanshi4@gmail.com>
2024-11-14 00:19:07 -0700

    Added collision detection to deal damage in enemy character

--- Assets/Scripts/enemyAIPatrol.cs
+++ Assets/Scripts/enemyAIPatrol.cs
@@ -6,17 +6,17 @@ using UnityEngine.AI;
 public class enemyAIPatrol : MonoBehaviour
 {
     GameObject player;
-
     NavMeshAgent agent;
 
     [SerializeField] LayerMask groundLayer, playerLayer;
-
     Animator animator;
+
+    // Box Collider for attack
+    BoxCollider boxCollider;
+
     // Patrol
     Vector3 destPoint;
-
     bool walkpointSet;
-    
     [SerializeField] float range;
 
     //state change
@@ -28,54 +28,83 @@ public class enemyAIPatrol : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.Find("Player");
         animator = GetComponent<Animator>();
+        boxCollider = GetComponentInChildren<BoxCollider>();
+        boxCollider.isTrigger = true; // Ensure it's set as a trigger
 
+        if (boxCollider == null)
+        {
+            Debug.LogWarning("BoxCollider not found or not set as trigger.");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        playerInSight  = Physics.CheckSphere(transform.position, sightRange, playerLayer);
-        playerInAttackRange  = Physics.CheckSphere(transform.position, attackRange, playerLayer);
+        playerInSight = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
         
-        if(!playerInSight && !playerInAttackRange) Patrol();
-    
-        if(playerInSight && !playerInAttackRange) Chase();
-        
-        if(playerInSight && playerInAttackRange) Attack();
+        if (!playerInSight && !playerInAttackRange) Patrol();
+        if (playerInSight && !playerInAttackRange) Chase();
+        if (playerInSight && playerInAttackRange) Attack();
     }
 
-    void Chase(){
+    void Chase()
+    {
         agent.SetDestination(player.transform.position);
     }
 
-    void Attack(){
-        Debug.Log("You are under attack :(");
-
-        if(!animator.GetCurrentAnimatorStateInfo(0).IsName("EnemyAttackAnimation")){
+    void Attack()
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("EnemyAttackAnimation"))
+        {
             animator.SetTrigger("Attack");
             agent.SetDestination(transform.position);
         }
-        
     }
 
     void Patrol()
     {
-        if(!walkpointSet) SearchForDest();
-        if(walkpointSet) agent.SetDestination(destPoint);
-        if(Vector3.Distance(transform.position, destPoint)< 10) walkpointSet = false;
+        if (!walkpointSet) SearchForDest();
+        if (walkpointSet) agent.SetDestination(destPoint);
+        if (Vector3.Distance(transform.position, destPoint) < 10) walkpointSet = false;
     }
 
-    void SearchForDest(){
+    void SearchForDest()
+    {
         float z = Random.Range(-range, range);
         float x = Random.Range(-range, range);
 
         destPoint = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
 
-        if(Physics.Raycast(destPoint, Vector3.down, groundLayer)){
+        if (Physics.Raycast(destPoint, Vector3.down, groundLayer))
+        {
             walkpointSet = true;
         }
+    }
 
+    void EnableAttack()
+    {
+        boxCollider.enabled = true;
+        Debug.Log("Enabled Attack");
+    }
 
+    void DisableAttack()
+    {
+        boxCollider.enabled = false;
+        Debug.Log("Disabled Attack");
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered by: " + other.name); // Check which object triggered the event
+
+        var player = other.GetComponent<PlayerMovement>();
+        if (player != null)
+        {
+            Debug.Log("HIT");
+        }
+        else
+        {
+            Debug.Log("Player not found");
+        }
     }
 }
